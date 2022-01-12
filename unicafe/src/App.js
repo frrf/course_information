3d6eb28a3007aa1453799cd42633734b3fd4d2cf
frrf <franciscopali71@gmail.com>
2022-01-11 21:29:10 -0800

    1.9 unicafe

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -9,16 +9,24 @@ const Button = ({handleClick, text}) => (
 const Section = ({text}) => (<h4>{text}</h4>)
 
 const Statistics = ({text1, score1,text2, score2,text3, score3,text4, score4,text5,score5,text6, score6}) => {
+  if (score1 === 0 && score2 === 0 && score3 === 0 && score4 === 0) {
+    return (
+      <>
+        <span>No Feedback given</span>
+      </>
+    )
+  } 
   return (
-  <>
-    <span>{text1} {score1}<br/></span>
-    <span>{text2} {score2}<br/></span>
-    <span>{text3} {score3}<br/></span>
-    <span>{text4} {score4}<br/></span>
-    <span>{text5} {score5}<br/></span>
-    <span>{text6} {score6}<br/></span>
-  </>
+    <>
+      <span>{text1} {score1}<br/></span>
+      <span>{text2} {score2}<br/></span>
+      <span>{text3} {score3}<br/></span>
+      <span>{text4} {score4}<br/></span>
+      <span>{text5} {score5}<br/></span>
+      <span>{text6} {score6}<br/></span>
+    </>
   )
+  
 }
 
 const App = () => {
