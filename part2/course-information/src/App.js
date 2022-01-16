fa79838e996664df5ca90461274bfc734b376d7d
frrf <franciscopali71@gmail.com>
2022-01-16 09:25:25 -0800

    2.5 Course Information

--- part2/course-information/src/App.js
+++ part2/course-information/src/App.js
@@ -1,50 +1,7 @@
 import React from 'react'
+import Course from './components/Course'
 
-const Header = ({courseName}) => (<h1>{courseName}</h1>)
-
-const Total = ({parts}) => {
-  const arr= parts.map(part=>part.exercises)
-  const reducer = (previousValue, currentValue) => previousValue + currentValue;
-  
-  return (
-    <>      
-      <p><strong>total of {arr.reduce(reducer)} exercises</strong></p>  
-    </>
-  )
-}
-
-const Content = ({parts}) => {
-  return (
-    <>
-      {parts.map(part => 
-        <Part key={part.id} part={part.name} exercise={part.exercises}
-      />)}
-    </>
-  )
-} 
-
-const Part = (props) => {
-  return (
-    <>
-      <p>
-        {props.part} {props.exercise}
-      </p>
-    </>
-  )
-}
-
-const Course = ({course}) => {
-  console.log(course)
-  return (
-      <>
-        <Header courseName={course.name}/>
-        <Content parts={course.parts}/>
-        <Total parts={course.parts}/>
-    </>
-  )
-}
-
-const App = () => {
+const App = ({}) => {
   const courses = [
     {
       name: 'Half Stack application development',
