e1cf69f6e4ff0d53c61a464039ab89876e9ab2c3
frrf <franciscopali71@gmail.com>
2022-01-17 21:23:41 -0800

    2.9 The Phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,10 +4,15 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '323-2323' }, {name: 'Ada Lovelace', number: '323-2323' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
+  const [userSearch, setUserSearch] = useState(false)
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -17,13 +22,17 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchChange = (event) => {
+    setNewSearch(event.target.value)
+    setUserSearch(true)
+  }
+
   const addEntries = (event) => {
     event.preventDefault()
-
     const checkName = obj => obj.name === newName
-
+    const len = persons.length
     if (!persons.some(checkName)) {
-      const nameObject = {name: newName, number:newNumber}
+      const nameObject = {name: newName, number:newNumber, id:len+1}
 
       if(newName.length === 0 || newNumber.length ===0) {
         alert('An input field is blank') 
@@ -37,9 +46,24 @@ const App = () => {
     setNewName('')
     setNewNumber('')
   }
+
+  // makes new array, ter conditional statement returns the new array into the searchNames variable
+  const searchNames = userSearch
+  ? persons.filter(person => person.name.toLowerCase().includes(newSearch.toLowerCase()))
+  : persons
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with 
+        <input
+          type="search"
+          value={newSearch}
+          onChange={handleSearchChange}
+        />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addEntries}>
         <div>
           name: 
@@ -61,8 +85,8 @@ const App = () => {
       </form> 
 
       <h2>Numbers</h2>
-      {persons.map(person => 
-        <span key={person.name}>{person.name} {person.number}<br/></span>
+      {searchNames.map(person => 
+        <span key={person.id}>{person.name} {person.number}<br/></span>
       )}
     </div>
   )
